cc7f323c5c9fb0f7dac58284b439df48b7fac896
Mateusz Łącki <matteo.lacki@gmail.com>
2013-04-16 00:25:10 +0200

    Before big one.

--- Scripts/testing_time_of_retrieving_data.R
+++ Scripts/testing_time_of_retrieving_data.R
@@ -3,9 +3,11 @@ time_internet <- system.time({
   load(con)
 })
 
-time_downloaded <- system.time(read.csv2("Data/time_of_downloading_over_the_internet.csv"))
+time_downloaded <- system.time(read.csv2("Data/prepared_data.csv"))
 
 times <- cbind(time_internet[3], time_downloaded[3])
 colnames(times) <- c("Downloading time", "Reading from file time") 
 
 write.csv2(times, "Data/time_of_downloading_over_the_internet.csv", row.names = FALSE)
+
+rm(diagnozaOsoby2011, time_downloaded, times, time_internet)
